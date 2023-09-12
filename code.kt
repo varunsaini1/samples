a424c97498ed78a3af21c60e2b4522f3b339c9b9
varunsaini-s <varun.s@surya-digital.com>
2023-09-12 10:21:04 +0530

    Adding updated file

--- code.kt
+++ code.kt
@@ -1,12 +1,32 @@
+//Defining Variables
 var a : Int = 5
 print ("Value of variable a : $a")
 
 var b = " value of a :"
 print("$b $a")
 
-fun valueinc(x: Int) : Int{
+var c : Int = 10
+print ("Value of variable c : $c")
+
+
+// Increment Function
+fun valueinc( x : Int) : Int{
     return x+=1
 }
 
+// using tthe functio
 a=valueinc(a)
 print("$b $a")
+
+//Print product of Two Numbers
+fun printProd(x: Int, y: Int) : Unit{
+    if(x != null && y != null){
+        print("Product of $x and $y : ${x * y}")
+    }
+    else{
+        print("Not Number value assigned.")
+    }
+}
+
+//Multiplying a and c
+printProd(a,c)
